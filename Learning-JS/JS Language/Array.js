9244ae2516a4ea7a098eedf1885bccaddce1b442
Swapnil Kumar <swapnilskumars@gmail.com>
2023-05-20 00:32:15 +0530

    added array

---
+++ Learning-JS/JS Language/Array.js
@@ -0,0 +1,13 @@
+// Arrays
+let books = ["book1", "book2", "book3"];
+
+// Interating through the list of books
+for(i in books){
+    console.log(books[i]);
+}
+
+// Getting a book by index
+console.log(books[0]);
+
+// Calling funtions inside books array
+console.log(books.length);
