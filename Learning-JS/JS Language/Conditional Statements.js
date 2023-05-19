90bbbfe4c1f66a871a0a50b3aa4d3a8848ecad84
Swapnil Kumar <swapnilskumars@gmail.com>
2023-05-20 00:32:05 +0530

    Added Statements

---
+++ Learning-JS/JS Language/Conditional Statements.js
@@ -0,0 +1,50 @@
+var a = 10;
+var b = 20;
+var key = "2";
+
+// If Else Conditional Statements
+if(a>b){
+    console.log("A is greater");
+}else{
+    console.log("B is greater");
+}
+
+// While Loop
+while (true) {
+    console.log("Playing");
+    if (a>b) {
+        break;
+    }
+    a++;
+}
+
+// Switch Statements
+switch (key) {
+    case 1:
+        console.log("first");
+        break;
+    case 2:
+        console.log("second");
+        break
+    default:
+        console.log("Other")
+        break;
+}
+//  For Loop
+for(let i = 1; i<=10; i++){
+    console.log(i);
+}
+
+// While Loop
+let i = 10;
+while(i>0){
+    console.log(i);
+    i--;
+}
+
+// Do While Loop
+do{
+    console.log(i+1);
+    i++;
+}while(i<10)
+
